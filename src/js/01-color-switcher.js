f49101105dadc84b7e78d2399fd85f8092cb47a3
Krzysztof Olejniczak <krzysztof.olejniczak@gmail.com>
2022-12-14 14:20:03 +0100

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+'use strict';
 
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const body = document.body;
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+const stopBtnDisable = () => {
+  stopBtn.disabled = true;
+};
+
+const backgroundColorChange = () => {
+  body.style.backgroundColor = getRandomHexColor();
+};
+
+const startColorChange = () => {
+  Notify.info('Starting background color change');
+  intervalId = setInterval(backgroundColorChange, 1000);
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
+};
+
+const stopColorChange = () => {
+  Notify.info('Background color change stopped');
+  clearInterval(intervalId);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+window.addEventListener('load', stopBtnDisable);
+startBtn.addEventListener('click', startColorChange);
+stopBtn.addEventListener('click', stopColorChange);
